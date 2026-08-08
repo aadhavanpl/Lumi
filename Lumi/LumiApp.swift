1e2b1e01775bfd529c722605c8dc7996a1f0df4c
Aadhavan <aadhavanlenin@gmail.com>
2026-08-08 10:20:22 +0530

    Initial Commit

---
+++ Lumi/LumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiApp.swift
+//  Lumi
+//
+//  Created by Aadhavan on 08/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
